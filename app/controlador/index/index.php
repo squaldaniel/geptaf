53eb113542c4eeb87a40706198b8192d14956ea4
Squaldaniel <daniel.santos.ap@gmail.com>
2017-12-31 16:11:59 -0200

    testando o helpme
    como seria se fosse possivel o framework se auto-explicar

--- app/controlador/index/index.php
+++ app/controlador/index/index.php
@@ -5,10 +5,8 @@ class index {
 		
 	}
 	public function index(){
-		$js = new jsincludes;
-		$js->helpme();
 		//echo "Ola mundo Marrento!!!";
-		//require_once(PATHMODELO . get_class()."_modelo/".get_class()."_modelo.php");
+		require_once(PATHMODELO . get_class()."_modelo/".get_class()."_modelo.php");
 	}
 }
 ?>
